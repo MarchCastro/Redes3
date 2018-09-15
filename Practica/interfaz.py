680ffbbdbe1b68249ffdfc1129aabd563537b349
MarchCastro <march.castrof@gmail.com>
2018-09-14 23:56:17 -0500

    Guardo cambios en interfaz y avance

--- Practica/interfaz.py
+++ Practica/interfaz.py
@@ -1,7 +1,19 @@
 import Tkinter
-root = Tkinter.Tk(  )
-for r in range(3):
-   for c in range(4):
-      Tkinter.Label(root, text='R%s/C%s'%(r,c),
-         borderwidth=1 ).grid(row=r,column=c)
-root.mainloop(  )
+from Tkinter import *
+from tkintertable.Tables import TableCanvas
+from tkintertable.TableModels import TableModel
+
+master = Tkinter.Tk()
+master.title("Bienvenido a casi Observium :)")
+master.geometry('400x400')
+tframe = Frame(master)
+tframe.pack()
+table = TableCanvas(tframe)
+table.createTableFrame()
+data = {'rec1': {'col1': 'kalkjajaalksjaldkjalkdancljdnsjvn', 'col2': 108.79, 'label': 'rec1'},
+'rec2': {'col1': 99.88, 'col2': 108.79, 'label': 'rec2'}}
+model = table.model
+model.importDict(data) #can import from a dictionary to populate model
+table.redrawTable()
+
+master.mainloop()
--- Practica/server.py
+++ Practica/server.py
@@ -4,16 +4,25 @@
 
 import socket
 import Tkinter
+import time
 import os 
 from Tkinter import *
 from tkMessageBox import *
 from pysnmp.hlapi import *
+from time import sleep
 
 ip = ''
 comunnity = ''
 port = ''
+version = ''
 agentCount = 0
 
+#Declaro mi ventana principal
+top = Tkinter.Tk()
+top.title("Bienvenido a casi Observium :)")
+top.geometry('400x400')
+
+fields = 'Hostname', 'Version SNMP', 'Puerto', 'Comunidad'
 
 def fetch(entries): #Recorre todos los datos que agregue y me los imprime en consola
 	showinfo('Agente creado!', 'Se ha creado correctamente un agente nuevo')
@@ -56,24 +65,27 @@ def addClient(): #Abre un recuadro a partir del recuadro principal y muestra su 
 def deleteClient():
 	tkMessageBox.showinfo( "Agregar un nuevo agente", "Hello World")
 
-def ping():
-	file = open("hosts.txt", "r")
-	#ip = ''
-	for linea in file.readlines():
-		global agentCount
-		agentCount = agentCount + 1 #Aqui esta mi contador 
-		palabras = linea.split(" ")
-		ip = palabras[0]
-		print ip
-		response = os.system("ping -c 1 " + ip)
-		# and then check the response...
-		if response == 0:
-			pingstatus = "Network Active"
-		else:
-			pingstatus = "Network Error"
-	print agentCount
-	#check_ping(ip)
-	file.close() 
+def getHostInfo():
+	while True:
+		file = open("hosts.txt", "r")
+		for linea in file.readlines():
+			global agentCount,ip,comunnity,port
+			agentCount = agentCount + 1 #Aqui esta mi contador 
+			palabras = linea.split(" ")
+			ip = palabras[0]
+			version = palabras[1]
+			port = palabras[2]
+			comunnity = palabras[3]
+			#print ip
+			#response = os.system("ping -c 1 " + ip)
+			# and then check the response...
+			#if response == 0:
+			#	pingstatus = "Network Active"
+			#else:
+			#	pingstatus = "Network Error"
+		#print agentCount
+		file.close() 
+		sleep(0.0005)
 
 def mib(): #No esta implementado aun
 	errorIndication, errorStatus, errorIndex, varBinds = next(
@@ -93,30 +105,39 @@ def mib(): #No esta implementado aun
 			print(' = '.join([x.prettyPrint() for x in varBind]))
 
 def main():
-	#Declaro mi ventana principal
-	top = Tkinter.Tk()
-	top.title("Bienvenido a casi Observium :)")
-	top.geometry('350x200')
+	while True:
+		file = open("hosts.txt", "r")
+		for linea in file.readlines():
+			global agentCount,ip,comunnity,port, version
+			agentCount = agentCount + 1 #Aqui esta mi contador 
+			palabras = linea.split(" ")
+			ip = palabras[0]
+			version = palabras[1]
+			port = palabras[2]
+			comunnity = palabras[3]
+		
+		add = Tkinter.Button(top, text ="Agregar agente", width=25, command = addClient).grid(row=0, column=0)
+		delete = Tkinter.Button(top, text ="Eliminar agente", width=25, command = addClient).grid(row=1, column=0)
+		agentInfo = Tkinter.Button(top, text ="Informacion de agente",width=25, command = deleteClient).grid(row=2, column=0)
 
-	fields = 'Hostname', 'Version SNMP', 'Puerto', 'Comunidad'
+		#getHostInfo()
+		print agentCount
+		#TITULO
+		colors = ['Dispositivos monitoreados','Numero de dispositivos', 'Status de dispositivos']
+		r = 3
+		for c in colors:
+			Label(text=c, width=25, fg='black').grid(row=r, column=0)
+			r = r+1
 
-	add = Tkinter.Button(top, text ="Agregar agente", command = addClient)
-	delete = Tkinter.Button(top, text ="Eliminar agente", command = ping)
-	agentInfo = Tkinter.Button(top, text ="Informacion de agente", command = deleteClient)
-
-	add.pack()
-	delete.pack()
-	agentInfo.pack()
-
-	ping() #Aqui estoy llamando a mi funcion 
-	#Aqui quiero plasmar mi numero que me de mi contador
-	w = Label(top, width=20, text="Numero de dispositivos: ", fg="black", anchor='w')
-	w.pack(padx=5, pady=10, side=LEFT)
-	w = Label(top, text=agentCount, fg="black")
-	w.pack(padx=5, pady=20, side=LEFT)
-
-	
-	top.mainloop()
+		
+		Label(text=agentCount, width=25, fg='black').grid(row=4, column=1)
+		Label(text=agentCount, width=25, fg='black').grid(row=5, column=1)
+		
+		
+		
+		file.close() 
+		sleep(0.0005)
+		mainloop()
 
 if __name__== '__main__': 
 	main()
