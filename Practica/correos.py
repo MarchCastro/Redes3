be843c1f784384870a8da31d0a3e9d314b18bb29
MarchCastro <march.castrof@gmail.com>
2018-10-18 23:22:29 -0500

    Guardo para envio de imagenes y envio de correo con gmail

--- Practica/correos.py
+++ Practica/correos.py
@@ -1,19 +1,36 @@
+import os
+import time
+import rrdtool
+import tempfile
+import smtplib
+from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
 from email.encoders import encode_base64
-import smtplib
 
 def enviaCorreo_LB(mensaje):
 	msg = MIMEMultipart()
-	msg['From']="asantiagom1401@alumno.ipn.mx" 
-	msg['To']="asantiagom1401@alumno.ipn.mx"
+	msg['From']='ruben.murga.d@gmail.com' 
+	msg['To']="march.castrof@gmail.com"
+	#msg['From']="asantiagom1401@alumno.ipn.mx" 
+	#msg['To']="asantiagom1401@alumno.ipn.mx"
 	msg['Subject']="Alerta - MiniObservium"
 	msg.attach(MIMEText(mensaje))
-	mailServer = smtplib.SMTP('smtp.office365.com',587)
+	png_file = '192.168.1.66-2-LB.png'
+    #print png_file
+	fp = open(png_file, 'rb')
+	img = MIMEImage(fp.read())
+	fp.close()
+	msg.attach(img)
+
+	mailServer = smtplib.SMTP('smtp.gmail.com',587)
+	#mailServer = smtplib.SMTP('smtp.office365.com',587)
 	mailServer.ehlo()
 	mailServer.starttls()
 	mailServer.ehlo()
-	mailServer.login("asantiagom1401@alumno.ipn.mx","7122871228")
-	mailServer.sendmail("asantiagom1401@alumno.ipn.mx", "asantiagom1401@alumno.ipn.mx", msg.as_string())
+	mailServer.login('ruben.murga.d@gmail.com',"aqolqmlxpyvfzcpy")
+	#mailServer.login("asantiagom1401@alumno.ipn.mx","7122871228")
+	mailServer.sendmail('ruben.murga.d@gmail.com', "march.castrof@gmail.com", msg.as_string())
+	#mailServer.sendmail("asantiagom1401@alumno.ipn.mx", "asantiagom1401@alumno.ipn.mx", msg.as_string())
 	mailServer.close() 
