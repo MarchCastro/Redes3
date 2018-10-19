675af8adc3e451de40f49ecc508cef882bb34288
MarchCastro <march.castrof@gmail.com>
2018-10-19 05:42:09 -0500

    sin correo

--- Practica/correos.py
+++ Practica/correos.py
@@ -13,8 +13,7 @@ def enviaCorreo_LB(mensaje):
 	msg = MIMEMultipart()
 	msg['From']='ruben.murga.d@gmail.com' 
 	msg['To']="march.castrof@gmail.com"
-	#msg['From']="asantiagom1401@alumno.ipn.mx" 
-	#msg['To']="asantiagom1401@alumno.ipn.mx"
+	
 	msg['Subject']="Alerta - MiniObservium"
 	msg.attach(MIMEText(mensaje))
 	png_file = '192.168.1.66-2-LB.png'
@@ -25,12 +24,12 @@ def enviaCorreo_LB(mensaje):
 	msg.attach(img)
 
 	mailServer = smtplib.SMTP('smtp.gmail.com',587)
-	#mailServer = smtplib.SMTP('smtp.office365.com',587)
+	
 	mailServer.ehlo()
 	mailServer.starttls()
 	mailServer.ehlo()
 	mailServer.login('ruben.murga.d@gmail.com',"aqolqmlxpyvfzcpy")
-	#mailServer.login("asantiagom1401@alumno.ipn.mx","7122871228")
+	
 	mailServer.sendmail('ruben.murga.d@gmail.com', "march.castrof@gmail.com", msg.as_string())
-	#mailServer.sendmail("asantiagom1401@alumno.ipn.mx", "asantiagom1401@alumno.ipn.mx", msg.as_string())
+	
 	mailServer.close() 
